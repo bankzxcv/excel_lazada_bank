496827cc38bf6f79dcd6a785ebff6e50e76e6c6a
Jirayu <bankzxcv@gmail.com>
2022-07-05 01:52:20 +0700

    remove second page

--- src/routes/excel.svelte
+++ src/routes/excel.svelte
@@ -6,14 +6,14 @@
   //   sp = new spreadsheet("#spread")
   // })
   // import xlsx from "xlsx"
-  import { merge } from "../stores/mergeCell"
-  import * as XLSX from "xlsx/xlsx.mjs"
+  // import { merge } from "../stores/mergeCell"
+  // import * as XLSX from "xlsx/xlsx.mjs"
   // merge.subscribe(e => {
   //   show = e
   // })
 </script>
 
 <div>
-  <div>{$merge ?? 0}</div>
+  <!-- <div>{$merge ?? 0}</div> -->
   <!-- <div class="w-screen h-screen" id="spread" /> -->
 </div>
